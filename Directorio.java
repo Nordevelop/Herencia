8a65dfa4f9166dcf5403f89da9a96405058a78a7
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-21 22:27:03 -0300

    Create Directorio.java

---
+++ Directorio.java
@@ -0,0 +1,25 @@
+public class Directorio extends Persona{
+  private String presidente;
+  private String ceo;
+  private String gerente;
+public String getPresidente() {
+	return presidente;
+}
+public void setPresidente(String presidente) {
+	this.presidente = presidente;
+}
+public String getCeo() {
+	return ceo;
+}
+public void setCeo(String ceo) {
+	this.ceo = ceo;
+}
+public String getGerente() {
+	return gerente;
+}
+public void setGerente(String gerente) {
+	this.gerente = gerente;
+}
+
+
+}
