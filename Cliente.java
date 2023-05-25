ac7d40a6f419241455de7345a632c05b69cbbfdd
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-24 22:41:05 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,27 +1,61 @@
 package misClases;
-import java.ClassCastException;
-import java.util.Date;
-public class Cliente extends Persona{
-	public Cliente(String nombre) {
-		super(nombre);
-		// TODO Apéndice de constructor generado automáticamente
-	}
-	private int idCliente;
-	private Date fechaRegistro;
-	private boolean vip;
-	public Date getFechaRegistro() {
-		return fechaRegistro;
-	}
-	public void setFechaRegistro(Date fechaRegistro) {
-		this.fechaRegistro = fechaRegistro;
-	}
-	public int getIdCliente() {
-		return idCliente;
-	}
-	public void setVip(boolean vip) {
-		this.vip = vip;
-	}
-	
-	
 
+import java.util.Date;
+
+public class Cliente extends Persona{
+    /*Propiedades de la clase Cliente
+     * con modificador de acceso Private
+     */
+    private int idCliente;
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int contadorCliente;
+    
+    public Cliente(Date fechaRegistro, boolean vip, String nombre, char genero, int edad, String direccion){ //metodo Constructor Cliente
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++Cliente.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+    /*Metodos 
+     *  Getters & Setters
+     */
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+     /*Metodos 
+     *  Boolean
+     */
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+   /*Metodo
+    *  mostrar en pantalla los objetos de la clase Cliente
+    */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
 }
