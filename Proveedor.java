99601e64197339fee8502acca292535ddb4c91b8
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-24 22:56:42 -0300

    Update Proveedor.java

--- Proveedor.java
+++ Proveedor.java
@@ -1,9 +1,29 @@
 package misClases;
-
+import java.util.Date;
 public class Proveedor extends Persona{
+  private int idProveedor;
+   private Date registro;
   private int cuit;
   private String razonsocial;
-  private Date registro;
+ 
+   private static int contadorProveedor;
+  public Proveedor(Date registro,int cuit, String razonsocial, String direccion){//metodo constructor
+     super(direccion);
+     this.idProveedor = ++Proveedor.contadorProveedor;
+     this.Registro = Registro;
+    this.cuit = cuit;
+  }
+  /*Metodos
+   * Getter & Setter
+   */
+   public Date registro() {
+        return registro;
+    }
+
+    public void registro(Date registro) {
+        this.registro = Registro;
+    }
+  
   
   
 }
