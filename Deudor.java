d50eccb2305dd57dd71c5df838dcbc75e26c4d90
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-27 18:51:02 -0300

    Create Deudor.java

---
+++ Deudor.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+package misClases;
+
+/*
+ * @author Nordevelop
+ *
+ */
+public class Deudor {
+	/*
+	 * Objetos encapsulados
+	 */
+	public Deudor() {//metodo constructor
+		
+	}
+
+}
