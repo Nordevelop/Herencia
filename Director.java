f6f1b78ac7f4e1173873023729b2db57638ecc10
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-27 00:14:14 -0300

    Update and rename Directorio.java to Director.java

---
+++ Director.java
@@ -0,0 +1,42 @@
+package misClases;
+
+public class Director extends Empleado {
+	
+	  private String categoria;
+	  public Director(String nombre, char genero, 
+			  int edad, String direccion, double sueldo, String categoria) {//parametros del metodo constructor
+			super(nombre, genero, edad, direccion, sueldo);
+			this.categoria=categoria;
+			/*
+			 * nombre, genero, edad, direccion
+			 * es objetos de la clase Persona
+			 * 
+			 * sueldo es objeto de la clase Empleado
+			 * categoria es objeto de la clase Director
+			 */
+			}
+	 
+
+	  		public String getCategoria() {
+	  			return categoria;
+	  		}
+
+
+	  		public void setCategoria(String categoria) {
+	  			this.categoria = categoria;
+	  		}
+
+
+	  		public String toString() {
+	  			StringBuilder sb = new StringBuilder();
+	  			sb.append("categoria = ").append(categoria);
+	  			sb.append(", ").append(super.toString());
+	  			sb.append('}');
+	  			return sb.toString();
+	  		}
+	  
+
+	
+
+}
+
